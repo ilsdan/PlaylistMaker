b4d32216115f0835d34fa08078d26747fe9e4a5d
ilsdan <me@ilsdan.ru>
2025-07-18 09:53:23 +0200

    Fix Parcelize

--- app/src/main/java/com/example/playlistmaker/search/domain/models/Track.kt
+++ app/src/main/java/com/example/playlistmaker/search/domain/models/Track.kt
@@ -1,8 +1,9 @@
 package com.example.playlistmaker.search.domain.models
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Track(
     val trackId: Long,
     var isFavorite: Boolean = false,
@@ -16,48 +17,4 @@ data class Track(
     val primaryGenreName: String,
     val country: String,
     val previewUrl: String?
-): Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readLong(),
-        parcel.readBoolean(),
-        parcel.readString(),
-        parcel.readString()!!,
-        parcel.readString(),
-        parcel.readLong(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()
-    )
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeLong(trackId)
-        dest.writeBoolean(isFavorite)
-        dest.writeString(trackName)
-        dest.writeString(artistName)
-        dest.writeString(trackTime)
-        dest.writeLong(trackTimeMillis)
-        dest.writeString(artworkUrl100)
-        dest.writeString(collectionName)
-        dest.writeString(releaseDate)
-        dest.writeString(primaryGenreName)
-        dest.writeString(country)
-        dest.writeString(previewUrl)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Track> {
-        override fun createFromParcel(parcel: Parcel): Track {
-            return Track(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Track?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+): Parcelable
